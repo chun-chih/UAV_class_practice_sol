2576e9fdf0c5d642dea2f91e3d6a676bc0378544
AMNshaw <75787424+AMNshaw@users.noreply.github.com>
2023-04-03 16:43:16 +0800

    add answer

--- class5/MinimumSnapTrajectory/getQ.m
+++ class5/MinimumSnapTrajectory/getQ.m
@@ -3,19 +3,16 @@ function Q = getQ(n_seg, n_order, ts)
     for k = 1:n_seg
         Q_k = [];
         T = ts(k);
-
-        %############################################################
-        %           Checkpoint1 : Finish the Q matrix
-        % Reminder
-        % parameters: Sigma7 Sigma6 Sigma5 Sigma4 Sigma3 Sigma2 Sigma1 Sigma0
-        % You cannot directly copy the class slide!
-
-        Q_k = [0];
-
-
-
-        %############################################################
-        
+        %#####################################################
+        % STEP 1.1: calculate Q_k of the k-th segment 
+        Q_k = [100800*T^7, 50400*T^6, 20160*T^5, 5040*T^4, 0, 0, 0, 0
+                50400*T^6, 25920*T^5, 10800*T^4, 2880*T^3, 0, 0, 0, 0
+                20160*T^5, 10800*T^4,  4800*T^3, 1440*T^2, 0, 0, 0, 0
+                 5040*T^4,  2880*T^3,  1440*T^2,  576*T^1, 0, 0, 0, 0
+                        0,         0,         0,        0, 0, 0, 0, 0
+                        0,         0,         0,        0, 0, 0, 0, 0
+                        0,         0,         0,        0, 0, 0, 0, 0
+                        0,         0,         0,        0, 0, 0, 0, 0];
         Q = blkdiag(Q, Q_k);
     end
 end
