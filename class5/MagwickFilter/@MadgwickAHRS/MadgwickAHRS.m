dc76119c3097326795e44d675d16fd2f6c31acc2
AMNshaw <75787424+AMNshaw@users.noreply.github.com>
2023-04-03 16:50:03 +0800

    add answer

--- class5/MagwickFilter/@MadgwickAHRS/MadgwickAHRS.m
+++ class5/MagwickFilter/@MadgwickAHRS/MadgwickAHRS.m
@@ -40,33 +40,31 @@ classdef MadgwickAHRS < handle
             h = quaternProd(q, quaternProd([0 Magnetometer], quaternConj(q)));
             b = [0 h(2) 0 h(4)];
 
-            %#####################################################
-            % Bonus : Determine the F and J matrix
-            %#####################################################
-
             % Gradient decent algorithm corrective step
-            
-            %F = [];
-            %J = [];
-           
+            F = [2*(q(2)*q(4) - q(1)*q(3)) - Accelerometer(1)
+                2*(q(1)*q(2) + q(3)*q(4)) - Accelerometer(2)
+                2*(0.5 - q(2)^2 - q(3)^2) - Accelerometer(3)
+                2*b(2)*(0.5 - q(3)^2 - q(4)^2) + 2*b(4)*(q(2)*q(4) - q(1)*q(3)) - Magnetometer(1)
+                2*b(2)*(q(2)*q(3) - q(1)*q(4)) + 2*b(4)*(q(1)*q(2) + q(3)*q(4)) - Magnetometer(2)
+                2*b(2)*(q(1)*q(3) + q(2)*q(4)) + 2*b(4)*(0.5 - q(2)^2 - q(3)^2) - Magnetometer(3)];
+            J = [-2*q(3),                 	2*q(4),                    -2*q(1),                         2*q(2)
+                2*q(2),                 	2*q(1),                    	2*q(4),                         2*q(3)
+                0,                         -4*q(2),                    -4*q(3),                         0
+                -2*b(4)*q(3),               2*b(4)*q(4),               -4*b(2)*q(3)-2*b(4)*q(1),       -4*b(2)*q(4)+2*b(4)*q(2)
+                -2*b(2)*q(4)+2*b(4)*q(2),	2*b(2)*q(3)+2*b(4)*q(1),	2*b(2)*q(2)+2*b(4)*q(4),       -2*b(2)*q(1)+2*b(4)*q(3)
+                2*b(2)*q(3),                2*b(2)*q(4)-4*b(4)*q(2),	2*b(2)*q(1)-4*b(4)*q(3),        2*b(2)*q(2)];
             step = (J'*F);
             step = step / norm(step);	% normalise step magnitude
 
-
             % Compute rate of change of quaternion
-            % Equation(30) in WEEK8 slide
-            qw_dot = 0.5 * quaternProd(q, [0 Gyroscope(1) Gyroscope(2) Gyroscope(3)]);
-            qDot =  qw_dot- obj.Beta * step';
+            qDot = 0.5 * quaternProd(q, [0 Gyroscope(1) Gyroscope(2) Gyroscope(3)]) - obj.Beta * step';
 
             % Integrate to yield quaternion
             q = q + qDot * obj.SamplePeriod;
             obj.Quaternion = q / norm(q); % normalise quaternion
         end
         function obj = UpdateIMU(obj, Gyroscope, Accelerometer)
-            %#####################################################
-            % IMU without the Magnetometer
-            %#####################################################
-            q = obj.Quaternion;
+            q = obj.Quaternion; % short name local variable for readability
 
             % Normalise accelerometer measurement
             if(norm(Accelerometer) == 0), return; end	% handle NaN
