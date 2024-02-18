4c32878a8d1c3a260e826cb06d02122ad9499d71
chun-chih <108335724+chun-chih@users.noreply.github.com>
2024-02-18 23:34:27 +0800

    Update number_pub.cpp
    add annotation

--- class1/src/number_pub.cpp
+++ class1/src/number_pub.cpp
@@ -3,6 +3,7 @@
 #include "std_msgs/Int32.h"
 
 using namespace std;
+// Create a message object
 std_msgs::Int32 msg;
 
 
@@ -11,8 +12,11 @@ int main(int argc, char **argv)
   cout << "initialize publish number" << endl;
 
   // you should rename your node and topic to avoid confliction
+  // Initialize the ROS node
   ros::init(argc, argv, "number_pub");
+  // Create a node handle
   ros::NodeHandle n;
+ // Create a publisher that publishes integers to the "number_1" topic
   ros::Publisher chatter_pub1 = n.advertise<std_msgs::Int32>("/number_1", 1000);
   ros::Rate loop_rate(10);
 
